f2f74e90ad1f541b095c0a7a9570f9af30806121
Nishan <nishan@hsenidoutsoursing.com>
2020-11-10 21:21:29 +0530

    User Profile Update done

--- app/src/main/java/com/tharuke/Edu_Chat/chatModule/ChatModule/ProfileUpdate.java
+++ app/src/main/java/com/tharuke/Edu_Chat/chatModule/ChatModule/ProfileUpdate.java
@@ -8,6 +8,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,75 +25,58 @@ import java.util.HashMap;
 
 public class ProfileUpdate extends AppCompatActivity {
 
-    MaterialEditText username, email, password;
+    MaterialEditText firstName, lastName;
     Button btn_update;
-
+    RadioButton radioButton;
+    RadioGroup radioGroup;
     FirebaseAuth auth;
     DatabaseReference reference;
+    FirebaseUser firebaseUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_update);
 
-        username = findViewById(R.id.username);
-        email = findViewById(R.id.regEmail);
-        password = findViewById(R.id.password);
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
         btn_update = findViewById(R.id.btn_update);
-        RadioButton radioButton2,radioButton3,radioButton4;
+        radioGroup = findViewById(R.id.radioGroup);
 
         auth = FirebaseAuth.getInstance();
 
         btn_update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String txt_username = username.getText().toString();
-                String txt_email = email.getText().toString();
-                String txt_password = password.getText().toString();
+                int selectedId = radioGroup.getCheckedRadioButtonId();
 
-                if(TextUtils.isEmpty(txt_username) || TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)){
+                radioButton = findViewById(selectedId);
+
+                String txt_firstName = firstName.getText().toString();
+                String txt_lastName = lastName.getText().toString();
+                if(TextUtils.isEmpty(txt_firstName) || TextUtils.isEmpty(txt_lastName)){
                     Toast.makeText(ProfileUpdate.this,"All fileds are required",Toast.LENGTH_SHORT).show();
-                }else if(txt_password.length() < 6){
-                    Toast.makeText(ProfileUpdate.this,"Password must be at least 6 characters",Toast.LENGTH_SHORT).show();
                 }else{
-//                    update(txt_username,txt_email,txt_password);
+                    update(txt_firstName,txt_lastName,radioButton.getText().toString());
                 }
 
             }
         });
     }
-//    private void update(final String username, String email, String password){
-//
-//        auth.updateCurrentUser(email, password)
-//                .addOnCompleteListener(task -> {
-//                    if(task.isSuccessful()){
-//                        FirebaseUser firebaseUser = auth.getCurrentUser();
-//                        assert firebaseUser != null;
-//                        String userid = firebaseUser.getUid();
-//
-//                        reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
-//
-//                        HashMap<String, String> hashMap = new HashMap<>();
-//                        hashMap.put("id", userid);
-//                        hashMap.put("username",username);
-//                        hashMap.put("imageUrl","default");
-//                        hashMap.put("status","offline");
-//                        hashMap.put("search",username.toLowerCase());
-//
-//                        reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>(){
-//                            @Override
-//                            public void onComplete(@NonNull Task<Void> task){
-//                                if(task.isSuccessful()){
-//                                    Intent intent= new Intent(ProfileUpdate.this, MainActivity.class);
-//                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-//                                    startActivity(intent);
-//                                    finish();
-//                                    Toast.makeText(ProfileUpdate.this,"Profile Updated",Toast.LENGTH_SHORT).show();
-//                                }
-//                            }
-//                        });
-//                    }else{
-//                        Toast.makeText(ProfileUpdate.this,"you can't Update with this email or password",Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//    }
+    private void update(final String firstName, String lastName,String radioValue){
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        assert firebaseUser != null;
+        String userId = firebaseUser.getUid();
+
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(userId);
+        HashMap<String,Object> hashMap = new HashMap<>();
+        hashMap.put("id",userId);
+        hashMap.put("firstName",firstName);
+        hashMap.put("lastName",lastName);
+        hashMap.put("gender",radioValue);
+
+        reference.updateChildren(hashMap);
+
+        Toast.makeText(ProfileUpdate.this,"Updated",Toast.LENGTH_SHORT).show();
+    }
 }
